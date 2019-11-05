58ccb08c69ca6746df3d8d8b00a45ae34ad2ae4d
Bermellet-Port <jnietomaldonado@gmail.com>
2019-11-05 18:31:03 +0100

    Añadida evaluación de expresiones básica

---
+++ app/Assets/Scripts/xNodeExtension/Nodes/Arithmetic/MathExpression.cs
@@ -0,0 +1,36 @@
+﻿using CodingSeb.ExpressionEvaluator;
+using NT.Atributes;
+using XNode;
+
+namespace NT.Nodes.Arithmetic
+{
+
+    [System.Serializable]
+    public class MathExpression : NTNode
+    {
+        public string expression;
+
+        [NTOutput] public float result;
+
+
+        public override object GetValue(NodePort port)
+        {
+            if (!string.IsNullOrEmpty(expression))
+            {
+                ExpressionEvaluator evaluator = new ExpressionEvaluator();
+                return evaluator.Evaluate(expression);
+            } else
+            {
+                return null;
+            }
+        }
+
+        public override string GetDisplayName()
+        {
+            return "Math Expression";
+        }
+
+
+    }
+
+}
