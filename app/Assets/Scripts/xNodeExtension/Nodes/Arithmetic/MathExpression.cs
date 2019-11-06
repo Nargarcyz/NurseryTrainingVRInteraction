f85ab7ec8951d8f91a647bd761d8483797e141c5
JN_Virvig <jnietomaldonado@gmail.com>
2019-11-06 17:53:12 +0100

    MathExpression cálculos decimales y variables usuario

--- app/Assets/Scripts/xNodeExtension/Nodes/Arithmetic/MathExpression.cs
+++ app/Assets/Scripts/xNodeExtension/Nodes/Arithmetic/MathExpression.cs
@@ -1,5 +1,6 @@
 ﻿using CodingSeb.ExpressionEvaluator;
 using NT.Atributes;
+using System.Collections.Generic;
 using XNode;
 
 namespace NT.Nodes.Arithmetic
@@ -10,19 +11,53 @@ namespace NT.Nodes.Arithmetic
     {
         public string expression;
 
-        [NTOutput] public float result;
+        [NTOutput] public double result;
 
 
         public override object GetValue(NodePort port)
         {
             if (!string.IsNullOrEmpty(expression))
             {
-                ExpressionEvaluator evaluator = new ExpressionEvaluator();
-                return evaluator.Evaluate(expression);
+                ExpressionEvaluator evaluator = customMathEvaluator();
+                
+
+                result = evaluator.Evaluate<double>(expression);
+                return result;
             } else
             {
+                result = 0;
                 return null;
             }
+        }
+
+        private ExpressionEvaluator customMathEvaluator()
+        {
+            ExpressionEvaluator evaluator = new ExpressionEvaluator();
+            // Set all necessary options
+            evaluator.OptionCaseSensitiveEvaluationActive = false;
+            evaluator.OptionForceIntegerNumbersEvaluationsAsDoubleByDefault = true;
+
+            evaluator.OptionNewFunctionEvaluationActive = false;
+            evaluator.OptionNewKeywordEvaluationActive = false;
+
+            // TODO: https://github.com/codingseb/ExpressionEvaluator/wiki/Options
+            // Quizas? OptionInlineNamespacesEvaluationActive, OptionFluidPrefixingActive
+
+            // Seguridad mediante bloqueo de tipos? https://github.com/codingseb/ExpressionEvaluator/wiki/C%23-Types-Management
+            //evaluator.TypesToBlock.Add();
+
+
+            // Add User Variables
+            SessionManager sm = SessionManager.Instance;
+            evaluator.Variables = new Dictionary<string, object>();
+
+            foreach (string key in sm.userVariables.Keys)
+            {
+                var value = sm.GetUserVariable(key);
+                evaluator.Variables.Add(key, value);
+            }
+
+            return evaluator;
         }
 
         public override string GetDisplayName()
