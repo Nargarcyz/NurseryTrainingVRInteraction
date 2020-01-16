59c9aec1945dc5922cf0514f85ff6eb0ab419232
JN_Virvig <jnietomaldonado@gmail.com>
2020-01-16 19:18:04 +0100

    Agrupar herramientas por filas y ordenadas

--- app/Assets/Scripts/xNodeExtension/Nodes/Session Core/ToolPlacementComparer.cs
+++ app/Assets/Scripts/xNodeExtension/Nodes/Session Core/ToolPlacementComparer.cs
@@ -53,7 +53,7 @@ namespace NT.Nodes.SessionCore
             var sgo = GetNodeGameObject() as SteelTableASceneGameObject;    // TODO: Cambiar por clase en capa superior (p.e. ToolSurfaceSceneGameObject --> (hereda) SteelTableASceneGameObject)
             if (sgo != null) {
                 GameObject surfaceGameobject = sgo.surface.gameObject;
-                List<GameObject> children = GetGameObjectChildren(surfaceGameobject);
+                var tools = GetGameObjectChildren(surfaceGameobject);
                 //children.Sort();
                 // The transforms of the GameObjects has its anchor point in the center (look tool.transform.position)
 
@@ -61,8 +61,11 @@ namespace NT.Nodes.SessionCore
                 BoxCollider bc = surfaceGameobject.GetComponent<BoxCollider>();
                 // Divide BoxCollider
                 int rowSize = GetRowSize(sgo.sceneObject);
-                var rowColliders = DivideColliderByRows(bc, rowSize);
-                
+                var rowColliders = DivideBoxColliderByRows(bc, rowSize);
+                var rowTools = ClassifyToolsByRows(tools, rowColliders);
+
+
+                RemoveColliders(rowColliders);
             }
 
             yield return null;
@@ -80,20 +83,20 @@ namespace NT.Nodes.SessionCore
             return SessionManager.Instance.GetSceneGameObject(nodeGraph.linkedNTVariable);
         }
 
-        private List<GameObject> GetGameObjectChildren(GameObject go)
+        private IEnumerable<GameObject> GetGameObjectChildren(GameObject go)
         {
-            List<GameObject> children = new List<GameObject>();
             foreach (Transform child in go.transform)
             {
-                children.Add(child.gameObject);
+                yield return child.gameObject;
             }
-            return children;
         }
 
         public override string GetDisplayName()
         {
             return "Tool Placement Comparer";
         }
+
+        #region Tool Ordering Logic
 
         private void UpdateOptionsList()
         {
@@ -109,13 +112,12 @@ namespace NT.Nodes.SessionCore
             var value = (SteelTableAData)data.GetValue();
             return value.rowSize;
         }
-        private List<BoxCollider> DivideColliderByRows(BoxCollider bc, int rowSize)
+        private IEnumerable<BoxCollider> DivideBoxColliderByRows(BoxCollider bc, int rowSize)
         {
             bool rowOnX = bc.size.x >= bc.size.z;
             float minCoord = rowOnX ? (bc.center.x - bc.size.x / 2) : (bc.center.z - bc.size.z / 2);
             float incrCoord = rowOnX ? (bc.size.x / rowSize) : (bc.size.z / rowSize);
 
-            var bcList = new List<BoxCollider>();
             for (int i = 0; i < rowSize; i++)
             {
                 BoxCollider row = bc.gameObject.AddComponent<BoxCollider>();
@@ -127,21 +129,67 @@ namespace NT.Nodes.SessionCore
                     row.center = new Vector3(bc.center.x, bc.center.y, minCoord + (i * incrCoord) + (incrCoord / 2));
                     row.size = new Vector3(bc.size.x, bc.size.y, bc.size.z / rowSize);
                 }
-                bcList.Add(row);
+                yield return row;
+            }
+        }
+
+        private List<List<GameObject>> ClassifyToolsByRows (IEnumerable<GameObject> Tools, IEnumerable<BoxCollider> BoxColliders)
+        {
+            var rowSize = Enumerable.Count(BoxColliders);
+            // Initialize result
+            List<List<GameObject>> result = new List<List<GameObject>>();
+            for (int i = 0; i < rowSize; i++)
+            {
+                result.Add(new List<GameObject>());
             }
 
-            return bcList;
+            // Classify tools
+            foreach (var tool in Tools)
+            {
+                for (int i = 0; i < rowSize; i++)
+                {
+                    BoxCollider bc = BoxColliders.ElementAt(i);
+                    Collider toolCollider = tool.GetComponentInChildren<Collider>();
+                    if (BoxColliderContainsPoint(bc, toolCollider.bounds.center, true))
+                    {
+                        result[i].Add(tool);
+                        break;
+                    }
+                }
+            }
+
+            // Sort tools by surface local axis
+            foreach (var row in result)
+            {
+                BoxCollider bc = BoxColliders.ElementAt(result.IndexOf(row));
+                bool rowOnX = bc.size.x >= bc.size.z;
+                Transform trans = bc.transform;
+                row.Sort((a, b) => rowOnX ? trans.InverseTransformPoint(a.transform.position).x.CompareTo(trans.InverseTransformPoint(b.transform.position).x)
+                                            : trans.InverseTransformPoint(a.transform.position).z.CompareTo(trans.InverseTransformPoint(b.transform.position).z));
+            }
+
+            return result;
         }
 
-        // Requires you to do all the transformations (centre, scale etc) on the parent gameobject
-        // rather than the collider component (so the collider centre is 0,0,0 and scale is 1,1,1)
-        private bool ColliderContainsPoint(Transform ColliderTransform, Vector3 Point)
+
+        private bool BoxColliderContainsPoint(BoxCollider boxCollider, Vector3 Point, bool ignoreY)
         {
-            Vector3 localPos = ColliderTransform.InverseTransformPoint(Point);
-            // Mirar size para hacerlo genérico?
-            return (Math.Abs(localPos.x) < 0.5f && Math.Abs(localPos.y) < 0.5f && Math.Abs(localPos.z) < 0.5f);
+            Vector3 localPos = boxCollider.transform.InverseTransformPoint(Point);
+            Vector3 halfSize = boxCollider.size / 2;
+            // Revisar < o <=
+            bool result = (Math.Abs(localPos.x) < halfSize.x && Math.Abs(localPos.z) < halfSize.z);
+            return ignoreY ? result : (result && Math.Abs(localPos.y) < halfSize.y);
         }
 
+        private void RemoveColliders(IEnumerable<Collider> colliders)
+        {
+            foreach (var collider in colliders)
+            {
+                Component.Destroy(collider);
+            }
+        }
+
+        #endregion
     }
 
 }
