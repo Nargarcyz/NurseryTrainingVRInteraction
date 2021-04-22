3617ebfd2a0344b7d59ce69c04d300f01912e55c
Oscar Garcia Toledo <narcyz2048@gmail.com>
2021-04-22 21:44:43 +0200

    Initial soap messages and fixed right hand collision

---
+++ app/Assets/Scripts/Exercise/Interactables/SoapScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using NT;
+
+public class SoapScript : MonoBehaviour
+{
+
+    private VRTK_InteractableObject linkedObject;
+    private GameObject otherHand = null;
+    private VRTK_SDKSetup setup;
+
+    protected virtual void OnEnable()
+    {
+        linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            linkedObject.InteractableObjectUngrabbed += (object sender, InteractableObjectEventArgs e) => { otherHand = null; };
+        }
+    }
+
+    private void Start()
+    {
+        setup = VRTK_SDKManager.GetLoadedSDKSetup();
+    }
+
+    private GameObject getController(GameObject o)
+    {
+        if (o.transform.IsChildOf(setup.actualLeftController.transform))
+        {
+            return setup.actualLeftController;
+        }
+        else if (o.transform.IsChildOf(setup.actualRightController.transform))
+        {
+            return setup.actualRightController;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+
+        var grabbingController = linkedObject.GetGrabbingObject();
+        Debug.Log("<color=red>Other Hand = " + otherHand.name + "</color>");
+
+        MessageSystem.SendMessage(otherHand.name + " soap");
+        // if (other.gameObject == setup.actualRightController)
+        // {
+
+        // }
+        // MessageSystem.SendMessage("Soap Used");
+        // gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        setup = VRTK_SDKManager.GetLoadedSDKSetup();
+        var controller = getController(other.gameObject);
+        Debug.Log(controller);
+
+        if (controller != null && controller != linkedObject.GetGrabbingObject())
+        {
+            otherHand = controller;
+            Debug.Log(otherHand);
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        setup = VRTK_SDKManager.GetLoadedSDKSetup();
+        var controller = getController(other.gameObject);
+
+        // Debug.Log(setup.actualLeftController);
+        if (controller != null && controller != linkedObject.GetGrabbingObject())
+        {
+            otherHand = null;
+        }
+    }
+}
--- app/Assets/Scripts/Scene/SceneGameObjects/SinkSceneGameObject.cs
+++ app/Assets/Scripts/Scene/SceneGameObjects/SinkSceneGameObject.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 using NT;
-
+using System.Linq;
+using System;
 public class SinkSceneGameObject : SceneGameObject
 {
     private GameObject boundaries;
+    private GameObject faucets;
+    private bool leftHandSoapy = false;
+    private bool rightHandSoapy = false;
+
+    private bool leftHandWashed = false;
+    private bool rightHandWashed = false;
+
+    private bool leftHandDried = false;
+    private bool rightHandDried = false;
+
 
     protected virtual void OnEnable()
     {
@@ -23,6 +34,8 @@ public class SinkSceneGameObject : SceneGameObject
         MessageSystem.onMessageSent += RecieveMessage;
     }
 
+    public ParticleSystem soapEffect;
+
     private void RecieveMessage(string msg)
     {
         if (msg.Contains("Exercise Started"))
@@ -33,12 +46,25 @@ public class SinkSceneGameObject : SceneGameObject
                 setup.actualBoundaries.transform.position = transform.position;
             }
         }
+        else if (msg.Contains("LeftHandAnchor soap"))
+        {
+            VRTK_SDKSetup setup = VRTK_SDKManager.GetLoadedSDKSetup();
+            var soapParticles = Instantiate(soapEffect);
+            soapParticles.transform.position = setup.actualLeftController.transform.position;
+        }
+        else if (msg.Contains("RightHandAnchor soap"))
+        {
+            VRTK_SDKSetup setup = VRTK_SDKManager.GetLoadedSDKSetup();
+            var soapParticles = Instantiate(soapEffect);
+            soapParticles.transform.position = setup.actualRightController.transform.position;
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        VRTK_SDKSetup setup = VRTK_SDKManager.GetLoadedSDKSetup();
-        Debug.Log(setup.actualRightController.transform.rotation);
+        // VRTK_SDKSetup setup = VRTK_SDKManager.GetLoadedSDKSetup();
+        // Debug.Log(setup.actualRightController.transform.rotation);
 
         // if (setup != null)
         // {
